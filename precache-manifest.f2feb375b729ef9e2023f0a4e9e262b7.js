1858e6ef3afd18bff6ef3e81fd1733848de3abdd
helloheatherw <heather.williams@teachfirst.org.uk>
2020-05-04 17:45:46 +0100

    Updates

---
+++ precache-manifest.f2feb375b729ef9e2023f0a4e9e262b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e97ee323f487936df99400af0e51aaf",
+    "url": "/todo_react_application/index.html"
+  },
+  {
+    "revision": "547f2a62524baca5c4b3",
+    "url": "/todo_react_application/static/css/main.9b269817.chunk.css"
+  },
+  {
+    "revision": "4e231e981f0129a56968",
+    "url": "/todo_react_application/static/js/2.0f242194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react_application/static/js/2.0f242194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547f2a62524baca5c4b3",
+    "url": "/todo_react_application/static/js/main.41261c3c.chunk.js"
+  },
+  {
+    "revision": "5d4c1a4e2e9a73a8502e",
+    "url": "/todo_react_application/static/js/runtime-main.606d0e57.js"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/todo_react_application/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "d033bbc0fbeb5e22b7b5452ac2822349",
+    "url": "/todo_react_application/static/media/confirm.d033bbc0.svg"
+  },
+  {
+    "revision": "32dc288af3f94d58d44089644c804b8f",
+    "url": "/todo_react_application/static/media/plus.32dc288a.svg"
+  }
+]);
