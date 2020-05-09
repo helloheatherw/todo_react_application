c4fe7f2031e33b77d4fbb7b69d11594419593355
helloheatherw <heather.williams@teachfirst.org.uk>
2020-05-09 20:00:00 +0100

    Add date input field and minor styling tweaks

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -8,12 +8,13 @@ function AddTask() {
   return (
       <Fragment>
         <form className="form-inline add-task">
-            <label className="sr-only" htmlFor="taskInput">Task</label>
-            <input type="text" className="form-control add-task__input-field" id="taskInput" placeholder="Something I need to do..."/>
+            <label className="sr-only" htmlFor="taskInput">Add new task</label>
+            <input type="text" className="form-control add-task__input-field add-task--text" id="taskInput" placeholder="Something I need to do..."/>
+            <input type="date" className="form-control add-task__input-field add-task--date"/>
             <button type="submit" className="btn btn-primary add-task__add-button">Add</button>
         </form> 
 
-        <button aria-label="add task" className="add-task--floating-button">
+        <button aria-label="add new task" className="add-task--floating-button">
             <img src={ addIcon } className="add-svg" alt=""/>
         </button>
       </Fragment>
